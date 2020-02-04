ab5f3f9c4ea896fc4fe6d329b7675c19a7971c75
Pierre Sassoulas <psassoulas@antidot.net>
2020-02-04 10:17:15 +0100

    Permit to use a client or url/login/password

--- copro/html_connector/html_to_published_fluid_api.py
+++ copro/html_connector/html_to_published_fluid_api.py
@@ -1,29 +1,49 @@
 import logging
 from pathlib import Path
+from typing import Optional
 
-from fluidtopics.connector import LoginAuthentication, RemoteClient
+from fluidtopics.connector import Client, LoginAuthentication, RemoteClient
 
 from copro.html_connector.html_to_fluid_api import html_to_fluid_api
 
 LOGGER = logging.getLogger(__name__)
 
+HTML_CONNECTOR_SOURCE_ID = "HTMLConnector"
+
 
 def html_to_published_fluid_api(
-    html_path: str, url: str, login: str, password: str, use_ftml: bool = False, metadatas: list = None
+    html_path: str,
+    url: Optional[str] = None,
+    login: Optional[str] = None,
+    password: Optional[str] = None,
+    source_id: Optional[str] = HTML_CONNECTOR_SOURCE_ID,
+    use_ftml: Optional[bool] = False,
+    metadatas: Optional[list] = None,
+    client: Optional[Client] = None,
 ):  # pylint: disable=too-many-arguments
+    """ Publish an html file to FT. """
+    if client is not None:
+        if url or login or password:
+            raise ValueError("Cannot use both url/login/password and a Client '{}'.".format(client))
+        if source_id != HTML_CONNECTOR_SOURCE_ID:
+            raise ValueError(
+                "Cannot use the source_id parameter '{}' and a Client '{}' at the same time.".format(source_id, client)
+            )
+    if client is None:
+        if not (url and login and password):
+            raise ValueError("Need at least url and login and password or a Client, got neither")
+        client = RemoteClient(url=url, authentication=LoginAuthentication(login, password), source_id=source_id)
     if metadatas is None:
         metadatas = []
     title = Path(html_path).name.replace(".html", "")
     publications = html_to_fluid_api(html_path, title, use_ftml, metadatas)
-    send_to_ft(url, login, password, publications)
+    send_to_ft(client, publications)
 
 
-def send_to_ft(url, login, password, publications):
-    source_id = "HTMLConnector"
-    client = RemoteClient(url=url, authentication=LoginAuthentication(login, password), source_id=source_id)
+def send_to_ft(client, publications):
     response = client.publish(publications)
-    if response.status_code == 404 and source_id in response.content.decode("utf8"):
-        if url.endswith("/"):
-            url = url[:-1]
+    if response.status_code == 404 and client.source_id in response.content.decode("utf8"):
+        if client.url.endswith("/"):
+            url = client.url[:-1]
         url = "{}{}".format(url, "/admin/khub/sources")
-        LOGGER.error("Please create an 'external' source with the ID '%s' here '%s'", source_id, url)
+        LOGGER.error("Please create an 'external' source with the ID '%s' here '%s'", client.source_id, url)
