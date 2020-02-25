067de0c196caa48c46344af6df6b1c31849ee8dd
Pierre Sassoulas <psassoulas@antidot.net>
2020-02-25 14:46:34 +0100

    fix a problem with the error raising

--- antidot/connector/html/external_source_id_does_not_exists_error.py
+++ antidot/connector/html/external_source_id_does_not_exists_error.py
@@ -1,8 +1,9 @@
 class ExternalSourceIdDoesNotExistsError(Exception):
     def __init__(self, client):
-        if client.url.endswith("/"):
-            url = client.url[:-1]
-        url = "{}{}".format(url, "/admin/khub/sources")
+        print(client.__dict__)
+        if client._sender.url.endswith("/"):
+            url = client._sender.url[:-1]
+        url = "{}{}".format(url, "admin/khub/sources/create")
         msg = "Please create an 'external' source with the ID '{}' at this URL '{}'".format(
             client._sender.source_id, url
         )
