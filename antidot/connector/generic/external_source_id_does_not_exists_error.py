7f335ddd68ca15546b50f581a890cc74538fb230
Benjamin Harpin <bharpin@antidot.net>
2020-05-06 16:05:38 +0200

    Bug fix when the address do not hand with a slash

--- antidot/connector/generic/external_source_id_does_not_exists_error.py
+++ antidot/connector/generic/external_source_id_does_not_exists_error.py
@@ -1,7 +1,8 @@
 class ExternalSourceIdDoesNotExistsError(Exception):
     def __init__(self, client):
-        if client._sender.url.endswith("/"):
-            url = client._sender.url[:-1]
+        url = client._sender.url
+        if url.endswith("/"):
+            url = url[:-1]
         url = "{}{}".format(url, "/admin/khub/sources/create")
         msg = "Please create an 'external' source with the ID '{}' at this URL '{}'".format(
             client._sender.source_id, url
